7ac69c51292455921ba739d9e31f57b45d99f308
Jayesh Patil <patiljayesh95666@gmail.com>
2025-07-15 11:15:11 +0530

    enable cors

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,20 @@ const cors = require("cors");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://learning-mgnt-sys.onrender.com",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
